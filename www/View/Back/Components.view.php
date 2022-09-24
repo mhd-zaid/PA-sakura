ce789f69fe7edabc7400d778e3372b51b9b867ab
Makan <makan.kamissoko@hotmail.fr>
2022-09-24 03:52:36 +0200

    Ajout de composants articles
    Article dashboard
    Article article

--- www/View/Back/Components.view.php
+++ www/View/Back/Components.view.php
@@ -4,7 +4,7 @@
 
 <section>
     <div class="grid">
-        <div class="row">
+        <!-- <div class="row">
             <div class="col col-sm-12 col-md-12 col-4">
                 <h3>Bouton</h3>
             </div>
@@ -24,6 +24,73 @@
                     <p>Titre</p>
                 </div>
             </div> 
+        </div> -->
+
+        <div class="row">
+            <article class="col col-4 col-md-4 col-sm-8 article article-dashboard">
+                <header>
+                    <img src="https://img.freepik.com/vecteurs-libre/fond-silhouettes-palmiers-colores_23-2148541792.jpg?w=2000" alt="">
+                </header>
+                <div class="article-container">
+                    <div>
+                        dd/mm/yyyy
+                        <span>
+                            <div class="badge">
+                                15<iconify-icon icon="bx:like"></iconify-icon>
+                            </div>
+                            <div class="badge">
+                                15<iconify-icon inline icon="fa-regular:comment"></iconify-icon>
+                            </div>
+                            <div class="badge">
+                                15<iconify-icon inline icon="bx:share"></iconify-icon>
+                            </div>
+                        </span>
+                    </div>
+                    <p class="article-label">Titre de l'article</p>
+                    <p class="article-label">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, natus nemo architect</p>
+                    <button class="cta-button btn--pink-rounded btn-category">Catégorie</button>
+                </div> 
+            </article>
         </div>
+        <br>
+        
     </div>
+</section>
+
+<section>
+        <div class="row">
+            <article class="col col-10 col-sm-12 article article-article">
+                <div class="row article-publication">
+                    <div class="col col-3 col-sm-12 article-img">
+                        <img src="https://img.freepik.com/vecteurs-libre/fond-silhouettes-palmiers-colores_23-2148541792.jpg?w=2000" alt="">
+                    </div>
+                    <div class="col col-6 col-sm-6 article-desc">
+                        <p class="article-label">Nom de la page</p>
+                        <p class="article-label">Nom de l'article</p>
+                        <p class="article-label">Nom de l'auteur</p>
+                    </div>
+                    <div class="col col-3 col-sm-6 article-date-comment">
+                        <p class="article-label">date de publication</p>
+                        <div>
+                            <span>
+                                <div class="badge badge-like">
+                                    15<iconify-icon icon="bx:like"></iconify-icon>
+                                </div>
+                                <div class="badge badge-comment">
+                                    15<iconify-icon inline icon="fa-regular:comment"></iconify-icon>
+                                </div>
+                                <div class="badge badge-share">
+                                    15<iconify-icon inline icon="bx:share"></iconify-icon>
+                                </div>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row article-action">
+                    <button class="col col-4 col-sm-4 btn--article article-publish">Publier</button>
+                    <button class="col col-4 col-sm-4 btn--article article-delete">Supprimer</button>
+                    <button class="col col-4 col-sm-4 btn--article article-unpublish">Retirer</button>
+                </div>
+            </article>
+        </div>
 </section>
