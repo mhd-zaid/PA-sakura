da406e780a8cd088abe1f39c82b912f9b9d9f7ed
Makan <makan.kamissoko@hotmail.fr>
2022-09-29 19:17:55 +0200

    fix : modification de la taille du container-fluid pour les pages de paramètres
    fix : modification de la taille du container-fluid pour les pages de paramètres

--- www/View/Back/Components.view.php
+++ www/View/Back/Components.view.php
@@ -1,3 +1,4 @@
+<div class="container-fluid container-fluid-parameters">
 <section>
     <h1>Components élèments d'édition</h1>
 </section>
@@ -129,7 +130,7 @@
 
 <section class="grid grid-article-elmt">
         <div class="row">
-            <article class="col col-3 article article-elmt">
+            <article class="col col-4 article article-elmt">
                 <div class="row">
                     <div class="col col-4 article-elmt-img">
                         <img src="https://img.freepik.com/vecteurs-libre/fond-silhouettes-palmiers-colores_23-2148541792.jpg?w=2000" alt="">
@@ -236,3 +237,25 @@
             </article>
         </div>
 </section>
+</div>
+<nav id="parameters-nav">
+    <h1 id="menu-title">Paramètres</h1>
+    <div class="container">
+        <h1>Général</h1>
+		<ul>
+            <li class="choices-parameters"><a href="/parametres-gestion-compte"><img src="Public/img/Back/account1.svg" alt="Account">Gestion du compte</a></li>
+            <li class="choices-parameters"><a href="/parametres-gestion-utilisateurs"><img src="Public/img/Back/site1.svg" alt="Utilisateurs">Gestions des utilisateurs</a></li>
+            <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/lang1.svg" alt="Language">Langue</a></li>
+            <li class="choices-parameters"><a href="/home"><img src="Public/img/Back/security1.svg" alt="Shield">Sécurité</a></li>
+		</ul>
+        <h1>Communication</h1>
+        <ul>
+            <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/notification1.svg" alt="Notification"><button class="cta-button btn--white-rounded">Notification</button></a></li>
+        </ul>
+        <h1>Aide</h1>
+        <ul>
+            <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/help1.svg" alt="Support">Support</a></li>
+            <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/cgu1.svg" alt="CGU">CGU</a></li>
+        </ul>
+	</div>
+</nav>
--- www/View/Back/Extension.view.php
+++
@@ -1,1 +0,0 @@
-<H1>hELLO wORLD</H1>
