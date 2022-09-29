07242fcfb4b239e64eeb8b4e5ddee1ca3e2e2574
kidax <axel.halifa0@gmail.com>
2022-09-29 14:13:11 +0200

    Page paramètres finis, debut de la page de la gestion des utilisateurs

--- www/Controller/Parameters.class.php
+++ www/Controller/Parameters.class.php
@@ -4,9 +4,25 @@ namespace App\Controller;
 
 use App\Core\View;
 
-class Parameters{
-    public function index(){
-            $v = new View("Page/Parameters", "Back");
-        
+class Parameters
+{
+    public function index()
+    {
+        session_start();
+        if (!isset($_SESSION['email'])) {
+        header("Location: /se-connecter");
+        } else {
+        $v = new View("Page/Parameters", "Back");
+        }
+    }
+
+    public function AccountManagement()
+    {
+        session_start();
+        if (!isset($_SESSION['email'])) {
+        header("Location: /se-connecter");
+        } else {
+        $v = new View("Page/AccountManagement", "Back");
+        }
     }
 }
---
+++ www/View/Page/UsersManagement.view.php
@@ -0,0 +1,7 @@
+<section class="grid">
+    <div class="row">
+        <span class="row">
+            <a href="Parametres/"> Paramètres </a><p> > Gestion des utilisateurs </p>
+        </span>
+    </div>
+</section>
