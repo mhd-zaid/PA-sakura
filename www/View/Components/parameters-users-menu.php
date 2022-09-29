53baadac55e95f565f51a709ccaeb2f89a1f59f2
Makan <makan.kamissoko@hotmail.fr>
2022-09-29 23:00:14 +0200

    feat : création du menu de paramètre utilisateur
    feat : création du menu de paramètre utilisateur

---
+++ www/View/Components/parameters-users-menu.php
@@ -0,0 +1,50 @@
+<nav id="parameters-users-nav">
+    <div class="grid">
+        <!-- <div class="container"> -->
+        <div class="row">
+            <a href="#" id="user-edit" >Modifier&nbsp;&nbsp;&nbsp;<span><iconify-icon icon="akar-icons:edit" style="color: white; font-size: 32px;"></iconify-icon></span></a>
+        </div>
+        <div class="row">
+            <div class="col col-12 center-text">
+                <img class="photo-profil" src="Public\img\Back\avatar.svg" alt="">
+                <p>Daniel Casanova</p>
+                <button class="cta-button btn--pink-rounded btn-category">Admin</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-12">
+                <p>Dernière connexion</p>
+                <p id="user-lastConnection">Aujourd'hui</p>
+            </div>
+        </div><hr>
+        <div class="row">
+            <div class="col col-3">
+                <p class="user-label">Rôles</p>
+                <p id="user-role">Admin</p>
+            </div>
+            <div class="col col-3">
+                <p class="user-label">Age</p>
+                <p id="user-age">32</p>
+            </div>
+            <div class="col col-3">
+                <p class="user-label">Sexe</p>
+                <p id="user-gender">Masculin</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-12">
+                <p class="user-label">Email</p>
+                <p id="user-email">dcasonova@hotmail.fr</p>
+            </div>
+        </div><hr>
+        <div class="row">
+            <div class="col col-12">
+                <p class="user-label">Notes</p>
+                <div id="user-notes">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae est voluptatem soluta incidunt, magnam aspernatur.
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- </div> -->
+</nav>
