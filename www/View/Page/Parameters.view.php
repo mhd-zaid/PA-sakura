5f7514b1d4bc7c5a2c3491af69f9b65719c4cb8b
Makan <makan.kamissoko@hotmail.fr>
2022-10-02 17:57:05 +0200

    fix : mis à jour de l'affichage de la page paramètres
    fix : mis à jour de l'affichage de la page paramètres

--- www/View/Page/Parameters.view.php
+++ www/View/Page/Parameters.view.php
@@ -4,46 +4,50 @@
         <p>Gérer les réglages</p>
     </div>
 </section>
-<section class="grid flex-col list-parameters">
-    <div class="col col-8 box-parameters">
-        <div class="col-12 box-title">
-            <h2> Général</h2>
-        </div>
-        <div class="col-12 box-choices">
-            <h3> Gérer les informations relatives à vos sites</h3>
-            <ul class="flex-col">
-                <li class="choices-parameters"><a href="/parametres-gestion-compte"><img src="Public/img/Back/Parameters/Account.svg" alt="Account">Gestion du compte</a></li>
-                <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/Sites.svg" alt="Sites">Gestions des sites</a></li>
-                <li class="choices-parameters"><a href="/parametres-gestion-utilisateurs"><img src="Public/img/Back/Parameters/Sites.svg" alt="Sites">Gestions des utilisateurs</a></li>
-                <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/Language.svg" alt="Language">Langue</a></li>
-                <li class="choices-parameters"><a href="/home"><img src="Public/img/Back/Parameters/Shield.svg" alt="Shield">Sécurité</a></li>
-            </ul>
-        </div>
-    </div>
-
-    <div class="col col-8 box-parameters">
-        <div class="col-12 box-title">
-            <h2> Communication</h2>
-        </div>
-        <div class="col-12 box-choices">
-            <h3> Gérer les informations relatives à vos sites</h3>
-            <ul class="flex-col">
-                <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/Notification.svg" alt="Notification">Notification</a></li>
-                <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/MessageCenter.svg" alt="MessageCenter">Centre de messagerie</a></li>
-            </ul>
+<section class="grid grid-rounded list-parameters">
+    <div class="row flex-row flex-row-center">
+        <div class="col col-8 box-parameters">
+            <div class="col-12 box-title">
+                <h2> Général</h2>
+            </div>
+            <div class="col-12 box-choices">
+                <h3> Gérer les informations relatives à vos sites</h3>
+                <ul class="flex-col">
+                    <li class="choices-parameters"><a href="/parametres-gestion-compte"><img src="Public/img/Back/Parameters/Account.svg" alt="Account">Gestion du compte</a></li>
+                    <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/Sites.svg" alt="Sites">Gestions des sites</a></li>
+                    <li class="choices-parameters"><a href="/parametres-gestion-utilisateurs"><img src="Public/img/Back/Parameters/Sites.svg" alt="Sites">Gestions des utilisateurs</a></li>
+                    <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/Language.svg" alt="Language">Langue</a></li>
+                    <li class="choices-parameters"><a href="/home"><img src="Public/img/Back/Parameters/Shield.svg" alt="Shield">Sécurité</a></li>
+                </ul>
+            </div>
         </div>
     </div>
-
-    <div class="col col-8 box-parameters">
-        <div class="col-12 box-title">
-            <h2> Aide</h2>
+    <div class="row flex-row flex-row-center">
+        <div class="col col-8 box-parameters">
+            <div class="col-12 box-title">
+                <h2> Communication</h2>
+            </div>
+            <div class="col-12 box-choices">
+                <h3> Gérer les informations relatives à vos sites</h3>
+                <ul class="flex-col">
+                    <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/Notification.svg" alt="Notification">Notification</a></li>
+                    <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/MessageCenter.svg" alt="MessageCenter">Centre de messagerie</a></li>
+                </ul>
+            </div>
         </div>
-        <div class="col-12 box-choices">
-            <h3> Besoin d'aide</h3>
-            <ul class="flex-col">
-                <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/Support.svg" alt="Support">Support</a></li>
-                <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/CGU.svg" alt="CGU">CGU</a></li>
-            </ul>
+    </div>
+    <div class="row flex-row flex-row-center">
+        <div class="col col-8 box-parameters">
+            <div class="col-12 box-title">
+                <h2> Aide</h2>
+            </div>
+            <div class="col-12 box-choices">
+                <h3> Besoin d'aide</h3>
+                <ul class="flex-col">
+                    <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/Support.svg" alt="Support">Support</a></li>
+                    <li class="choices-parameters"><a href="/parametres"><img src="Public/img/Back/Parameters/CGU.svg" alt="CGU">CGU</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </section>
