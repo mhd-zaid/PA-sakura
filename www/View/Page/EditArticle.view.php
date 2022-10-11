77a5484f2625be950402f241f84110d0beb97d68
Makan <makan.kamissoko@hotmail.fr>
2022-10-11 19:54:17 +0200

    fix(Article) : Ajout de l'input pour le slug de l'article
    fix(Article) : Ajout de l'input pour le slug de l'article

--- www/View/Page/EditArticle.view.php
+++ www/View/Page/EditArticle.view.php
@@ -5,19 +5,27 @@
         <div class="col col-12">
             <h1 class="h1-section-back">Création d'un article</h1>
             <form action="" method="POST">
-            <textarea class="ckeditor" id="editor" name="editor"></textarea>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col col-12">
-            <button type="submit" name="submit" class="cta-button btn--pink" id="add">Ajouter</button>
-</form>
+                        <div class="row">
+                            <div>
+                                <p id="article-slug">Titre de l'article</p>
+                            </div>
+                            <div class="col col-5 flex-col flex-col-center">
+                                <input id="ipt-slug" type="text" name="article-slug">
+                            </div>
+                        </div>
+                    <textarea class="ckeditor" id="editor" name="editor"></textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col col-12">
+                    <button type="submit" name="submit" class="cta-button btn--pink" id="add">Ajouter</button>
+            </form>
         </div>
     </div>
 </section>
 
 <script>
-   ClassicEditor
+    ClassicEditor
     .create( document.querySelector( '#editor' ) )
     .then( editor => {
         theEditor = editor; // Save for later use.
