db0b487597b1d09b6b393c31d44ba368b39cff3b
Zaid MOUHAMAD <zaidmouhamad@gmail.com>
2022-12-22 14:08:06 +0100

    feat(apparence): convert json to css

--- www/Controller/Apparence.class.php
+++ www/Controller/Apparence.class.php
@@ -9,9 +9,11 @@ class Apparence{
         $v = new View("Page/Apparence", "Back");
         $css =  json_decode(file_get_contents(__DIR__."/../Public/css/site.json"));
         $cssTheme = $css->themes[0];
-        $selectorsWithValues = ["h2" => ["color" => "pink", "font-size" => "120px"], "h1" => ["color" => "blue"]];
+        // $selectorsWithValues = ["h1" => ["color" => "blue"]];
+        $selectorsWithValues = ["h1" => ["color" => "blue", "font-size" => "120px"]];
+        $this->changeValueCss($cssTheme, $selectorsWithValues);
         // var_dump($this->changeValueCss($cssTheme, $selectorsWithValues));
-        die;
+        // die;
     }
 
     public function changeValueCss($css, $selectorsWithNewValues){
@@ -28,6 +30,15 @@ class Apparence{
                 }
             }
         }
+        $this->generateCss($css);
         return $css; 
+    }
+
+    public function generateCss($json) {
+        $json = str_replace(["\"","},",",",":{"],["","\n}\n",";\n\t","{\n\t"],substr(json_encode($json),1,-1));
+        
+        $filename = "site-theme-x.css";
+
+        \file_put_contents(__DIR__."/../Public/css/$filename",$json);
     }
 }
