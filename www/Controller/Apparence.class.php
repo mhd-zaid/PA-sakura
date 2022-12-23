39e9f64cf57ab3f24c8ff97eb5c6d2f9296ee201
kidax <axel.halifa0@gmail.com>
2022-12-23 18:07:40 +0100

    feat Apparence
    (Seulement pour h1)
    Récupération des valeurs du formulaire pour changer les valeurs du fichier site-theme-x.css

--- www/Controller/Apparence.class.php
+++ www/Controller/Apparence.class.php
@@ -9,30 +9,82 @@ class Apparence{
         $v = new View("Page/Apparence", "Back");
         $css =  json_decode(file_get_contents(__DIR__."/../Public/css/site.json"));
         $cssTheme = $css->themes[0];
-        // $selectorsWithValues = ["h1" => ["color" => "blue"]];
-        $selectorsWithValues = ["h1" => ["color" => "blue", "font-size" => "120px"]];
-        $this->changeValueCss($cssTheme, $selectorsWithValues);
-        // var_dump($this->changeValueCss($cssTheme, $selectorsWithValues));
-        // die;
+        $selectorsWithValues = ["h2" => ["color" => "pink", "font-size" => "120px"], "h1" => ["color" => "blue"]];
+        // $this->changeValueCss($cssTheme, $selectorsWithValues);
+        die;
     }
 
     public function changeValueCss($css, $selectorsWithNewValues){
-        foreach($css as $selectorJsonCss=>$valueJsonCss){ // boucle sur les selecteurs du json
-            foreach($selectorsWithNewValues as $selectors=>$newValue){ // boucle sur les selecteurs à modifiés
-                if($selectorJsonCss == $selectors){ // si le selecteur existe dans le json
-                    foreach ($valueJsonCss as $keyJson=>&$valueJson ) {  // boucle sur les valeurs du json
-                        foreach ($newValue as $keyCss => $newValueCss) { // boucle sur les valeurs à modifier
-                            if($keyJson == $keyCss){
-                                $valueJson = $newValueCss;
+        //obliger de pointer sur themes pour faire fonctionner la boucle
+        foreach($css->themes as $differentThemes=>$valueThemeCss){ // boucle sur les selecteurs du json
+            foreach($selectorsWithNewValues[0] as $selectors=>$newValue){ // boucle sur les selecteurs à modifiés, $selectorsWithNewValues[0] -> pour boucler sur les nouvelles données
+                if($differentThemes === $selectorsWithNewValues["theme-choice"]){ // On vérifie quel theme est choisie 
+                    $themeChoiced = $differentThemes; //On sauvegarde la valeur du theme choisis
+                    foreach($valueThemeCss as $selectorJsonCss => $valueJsonCss){ // On récupere les sélecteurs Json
+                        if($selectorJsonCss === $selectors){// si le selecteur existe dans le json
+                            foreach ($valueJsonCss as $keyValueCss => &$valueCss) {  // boucle sur les valeurs du json
+                                foreach ($newValue as $keyNewValue => $newValueSend) {// boucle sur les valeurs à modifier
+                                    if($keyValueCss === $keyNewValue){
+                                        if(!empty($newValueSend)){
+                                            $valueCss = $newValueSend;
+                                        }
+                                    }                                
+                                }                            
                             }
                         }
                     }
                 }
             }
         }
-        $this->generateCss($css);
+        
+        $this->generateCss($css->themes[$themeChoiced]);
         return $css; 
     }
+
+    // public function changeValueCss($css, $selectorsWithNewValues){
+    //     foreach($css as $selectorJsonCss=>$valueJsonCss){ // boucle sur les selecteurs du json
+    //         foreach($selectorsWithNewValues as $selectors=>$newValue){ // boucle sur les selecteurs à modifiés
+    //             if($selectorJsonCss == $selectors){ // si le selecteur existe dans le json
+    //                 foreach ($valueJsonCss as $keyJson=>&$valueJson ) {  // boucle sur les valeurs du json
+    //                     foreach ($newValue as $keyCss => $newValueCss) { // boucle sur les valeurs à modifier
+    //                         if($keyJson == $keyCss){
+    //                             $valueJson = $newValueCss;
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     print_r($css);
+    //     $this->generateCss($css);
+    //     return $css; 
+    // }
+    
+
+    //Fonction seulement pour h1
+    public function editH1(){
+        $selectorWithNewValues = [
+                                    "theme-choice"=>0,
+                                    [
+                                        "h1" => 
+                                        [
+                                        "color" => $_POST["h1-color"], 
+                                        "font-size" => $_POST["h1-font-size"],  
+                                        "font-weight" => $_POST["h1-font-weight"],  
+                                        "font-family" => $_POST["h1-font-family"]
+                                        ]
+                                    ]
+                                ]; 
+        
+        $css =  json_decode(file_get_contents(__DIR__."/../Public/css/site.json"));
+
+        $this->changeValueCss($css,$selectorWithNewValues);
+
+        header("Location: /apparence");
+                            
+    }
+
+
 
     public function generateCss($json) {
         $json = str_replace(["\"","},",",",":{"],["","\n}\n",";\n\t","{\n\t"],substr(json_encode($json),1,-1));
