e80239cb42f4cd46772868b0517e3a24a07ed5b8
Yikkme <nicky.thaing.etu@univ-lille.fr>
2022-12-28 02:01:56 +0100

    Nciky Push

--- www/View/Site/SingleArticle.view.php
+++ www/View/Site/SingleArticle.view.php
@@ -3,7 +3,7 @@
 	<div class="row flex-row-center">
 		<!-- Blog Entries Column (REPEAT THAT) -->
 		<?php
-			echo "<div class='col-md-8 '>
+		echo "<div class='col-md-8 '>
 			<div class='item-article'> 
 			<img src='/uploads/{$post[0]['Image_Name']}' alt=''>
 			<div class='content-box'> 
@@ -12,10 +12,20 @@
 			</div>
 			</div>
 			<hr>
-		</div>
 		";
-
 		?>
-
+		<div class="item-comment">
+			<h4>Commentaire</h4>
+			<form action="/action_page.php">
+				
+				<label for="fname">Pseudo:</label><br>
+				<input type="text" id="fname" name="fname"><br>
+				<label for="lname">E-mail:</label><br>
+				<input type="text" id="lcomment" name="lcomment"><br>
+				<label for="lcomment">Your Message:</label><br>
+				<input type="textarea" id="lcomment" name="lcomment" class="textarea-comments"><br>
+				<input class="button-comment" type="submit" value="Submit">
+			</form>
+		</div>
 	</div>
 </div>
