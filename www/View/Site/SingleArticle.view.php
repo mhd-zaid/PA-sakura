a5269c47c7520d19c8c9f92e5f6d8769e1cdb2f2
Makan Kamissoko <makan.kamissoko@hotmail.fr>
2022-12-30 16:06:22 +0100

    fix Article

--- www/View/Site/SingleArticle.view.php
+++ www/View/Site/SingleArticle.view.php
@@ -3,16 +3,16 @@
 	<div class="row flex-row-center">
 		<!-- Blog Entries Column (REPEAT THAT) -->
 		<?php
+		
 		echo "<div class='col-md-8 '>
-			<div class='item-article'> 
-			<img src='/uploads/{$post[0]['Image_Name']}' alt=''>
-			<div class='content-box'> 
+			<div class='item-article'> ";
+		if($post[0]['Image_Name'] != "UndefinedImage.jpg")	echo "<img src='/uploads/{$post[0]['Image_Name']}' alt=''>";
+		echo "<div class='content-box'> 
 			<p class='paragraph'><span></span> Posted on {$post[0]['Date_Created']}</p>
 			<p class='paragraph'>{$post[0]['Content']}</p>
 			</div>
 			</div>
-			<hr>
-		";
+			<hr>";
 		?>
 		<div class="item-comment">
 			<h4>Commentaire</h4>
