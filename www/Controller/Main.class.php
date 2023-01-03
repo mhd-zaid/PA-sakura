27829e15be2b0adb3b4bd0cb735319437bd281e0
Mouhamad Zaid <zaidmouhamad@gmail.com>
2023-01-03 12:11:54 +0100

    feat(Release): clear code

--- www/Controller/Main.class.php
+++ www/Controller/Main.class.php
@@ -114,11 +114,10 @@ class Main
 	public function download()
 	{
 		$file = __DIR__."/../test.zip";
-		$fileContent = file_get_contents($file);
+		header("Content: $file ");
 		header("Content-type: application/zip"); 
 		header("Content-Disposition: attachment; filename=sakura");
 		header("Content-length: " . filesize($file));
-		header("Content: $file ");
 		header("Pragma: no-cache"); 
 		header("Expires: 0"); 
 		readfile("$file");
