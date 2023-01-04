945cc6ca2401583ec23f9c7d154174c9d9ba26cd
Mouhamad Zaid <zaidmouhamad@gmail.com>
2023-01-04 16:09:54 +0100

    feat(Statistique): add statistique on preview

--- www/Controller/Admin.class.php
+++ www/Controller/Admin.class.php
@@ -11,20 +11,6 @@ class Admin
 
 	public function dashboard(): void
 	{
-		$idSession = session_id();
-		$stat = new statsModel();
-		if (!$stat->existSession($idSession)) {
-			$stat->setSession($idSession);
-			$objectDate = date_format(new DateTime(), "Y-m-d");
-			$stat->setDate($objectDate);
-			$stat->save();
-		} else {
-			$stat->setId($stat->findIdBySession($idSession));
-			$stat->setSession($idSession);
-			$objectDate = date_format(new DateTime(), "Y-m-d");
-			$stat->setDate($objectDate);
-			$stat->save();
-		}
-		$v = new View("Dashboard/Home");
+		$v = new View("Page/Home");
 	}
 }
