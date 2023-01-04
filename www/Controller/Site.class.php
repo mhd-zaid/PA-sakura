cd6476e1e28c38c7583071f152eb37bba3280433
Mouhamad Zaid <zaidmouhamad@gmail.com>
2023-01-04 17:06:40 +0100

    feat(Release): fix error comment

--- www/Controller/Site.class.php
+++ www/Controller/Site.class.php
@@ -67,78 +67,77 @@ class Site{
 				exit();
 			}
 		}
-
-
-		if (isset($_POST['signaler-comment'])){
+		if(isset($_POST['signaler-comment']))
+		{
 			$comment = new CommentModel();
-			$commentData = $comment->find($_POST['signaler-id']);
-            
-            if(in_array($_POST['signaler-id'],$_SESSION["messages_reported"])){
-              $_SESSION["flash-error"] = "Vous avez déjà signalé ce commentaire";
-			  exit();
-			}
+			$commentData = $comment->findCommentbyId($_POST['signaler-id']);
 			
-			$comment->setNbrSignalement($commentData['Nombre_signalement'] + 1); 
+			if(in_array($_POST['signaler-id'],$_SESSION["messages_reported"])){
+				$_SESSION["flash-error"] = "Vous avez déjà signalé ce commentaire";
+			}else{
+				$comment->setNbrSignalement($commentData['Nombre_signalement'] + 1); 
 
-			array_push($_SESSION["messages_reported"], $_POST['signaler-id']);
-			
-			if($comment->getNbrSignalement() === 5){
-				$comment->setStatus("unapprove");
-				exit();
+				array_push($_SESSION["messages_reported"], $_POST['signaler-id']);
+				
+				if($comment->getNbrSignalement() === 5){
+					$comment->setStatus("unapprove");
+					exit();
+				}
+				$comment->setId($_POST['signaler-id']);
+				$comment->setAuthor($commentData['Author']);
+				$comment->setContent($commentData['Content']);
+				$comment->setEmail($commentData['Email']);
+				$comment->setStatus("approved");
+				$today = date("Y-m-d");
+				$comment->setDateCreated($today);
+				$comment->setCommentPostId($_GET['id']);
+				$comment->save();
+				$signaler = new ModifyNotification();
+				$comment->subscribeToNotification($signaler);
+				$comment->update($_POST['signaler-id']);
+				$_SESSION["flash-success"] = "Vous avez bien signalé ce commentaire";
 			}
-            $comment->setId($_POST['signaler-id']);
-			$comment->setAuthor($commentData['Author']);
-            $comment->setContent($commentData['Content']);
-            $comment->setEmail($commentData['Email']);
-			$comment->setStatus("approved");
-			$today = date("Y-m-d");
-			$comment->setDateCreated($today);
-			$comment->setCommentPostId($_GET['id']);
-			$comment->save();
-			$signaler = new ModifyNotification();
-			$comment->subscribeToNotification($signaler);
-			$comment->update($_POST['signaler-id']);
-			$_SESSION["flash-success"] = "Vous avez bien signalé ce commentaire";
-			exit();
 		}
 	}
 	public function showSinglePost(): void
     {
-		if(!isset($_SESSION["messages_reported"])){
-			$_SESSION["messages_reported"] = [];
-		}
+        if(!isset($_SESSION["messages_reported"])){
+            $_SESSION["messages_reported"] = [];
+        }
         $post = new ArticleModel();
-		$comment = new CommentModel();
+        $comment = new CommentModel();
 
-		$formComment = $comment->formCommentaire();
+        $formComment = $comment->formCommentaire();
 
-		if(!empty($_POST)){
-		$data = [];
-		isset($_POST['author']) ? array_push($data, $_POST["author"]) : '';
-		isset($_POST['content']) ? array_push($data, $_POST["content"]) : '';
-		isset($_POST['email']) ? array_push($data, $_POST["email"]) : '';
-		$verificator = new Verificator($formComment, $data);
-		$configFormErrors = $verificator->getMsg();
-		if(empty($configFormErrors)){
-			if(isset($_POST['submit'])){
+        if(!empty($_POST)){
+			$data = [];
+			isset($_POST['author']) ? array_push($data, $_POST["author"]) : '';
+			isset($_POST['content']) ? array_push($data, $_POST["content"]) : '';
+			isset($_POST['email']) ? array_push($data, $_POST["email"]) : '';
+			$verificator = new Verificator($formComment, $data);
+			$configFormErrors = $verificator->getMsg();
+			if(isset($_POST['signaler-comment']))
+			{
 				$this->saveComment();
 			}
+			if(isset($_POST['submit'])){
+				if(empty($configFormErrors)){
+					$this->saveComment();
+				}
+			}
+		}
+		if (isset($_GET['id'])) {
+			$postData = $post->selectSingleArticle($_GET['id']);
+			$comments = $comment->selectApprovedComments($_GET['id']);
+		}
+		$v = new View("Site/SingleArticle", "Front2");
+		$v->assign("post", $postData);
+		$v->assign("comments", $comments);
+		$v->assign("configForm", $formComment);
+		if (isset($_POST['submit'])) {
+			$v->assign("configFormErrors", $configFormErrors??[]);
 		}
 	}
-        if (isset($_GET['id'])) {
-            $postData = $post->selectSingleArticle($_GET['id']);
-			$comments = $comment->selectApprovedComments($_GET['id']);
-            $v = new View("Site/SingleArticle", "Front2");
-            $v->assign("post", $postData);
-			$v->assign("comments", $comments);
-			$this->saveComment();
-        }
-	$v = new View("Site/SingleArticle", "Front2");
-	$v->assign("post", $postData);
-	$v->assign("comments", $comments);
-	$v->assign("configForm", $formComment);
-	$v->assign("configFormErrors", $configFormErrors??[]);
-}
 
 	public function showSinglePage(): void
     {	
